8b0615bd62434bb6fb2fb5dfeb12d2ed95cfde19
sjaakvandenberg <mail@svdb.co>
2015-07-09 16:52:10 +0200

    Add exercises 43-49

---
+++ 49_greet_me.rb
@@ -0,0 +1,15 @@
+# 49_greet_me.rb
+# http://www.codewars.com/kata/greet-me
+#
+# Sjaak van den Berg
+# http://www.codewars.com/users/sjaakvandenberg
+# @svdb
+
+# Write a method that takes one argument as name and then greets that
+# name, capitalized and ends with an exclamation point.
+
+def greet(name)
+  "Hello #{name.capitalize}!"
+end
+
+p greet('Riley') # "Hello Riley!"
